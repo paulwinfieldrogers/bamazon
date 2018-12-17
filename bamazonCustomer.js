81c66d511925b19f9d1d10e60380e29c9a3f4ba6
Paul Rogers <paulwrogers@yahoo.com>
2018-12-17 15:02:32 -0800

    all customer logic complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,9 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table');
 
-//instantiate
-
-// table is an Array, so you can `push`, `unshift`, `splice` and friends*/
+//connection info for mysql DB
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,7 +15,8 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-//var itemReturned = []
+//the below uses cli table to format the output - you push the response 
+//items into an array after iterating through the response
 
 var showAllItems = function () {
   connection.query("SELECT * FROM products", function (err, res) {
@@ -26,37 +25,36 @@ var showAllItems = function () {
       head: ['Product Id', 'Product Description', 'Cost'],
       colWidths: [5, 50, 8],
     });
-
     for (var i = 0; i < res.length; i++) {
       table.push([res[i].item_id, res[i].product_name, res[i].price]);
     }
-
     //console.log(res[0].item_id )
-      //console.log(res[0].item_id )
-        //console.log(res[0].item_id )
-   console.log(table.toString());
+    //console.log(res[1].item_id )
+    //console.log(res[2].item_id )
+    console.log(table.toString());
     customerChoice()
   });
 };
-
-//neet to get item to add into inquirer question
-
+//need to get item to add into inquirer question
+//here we request that the user enter an item id and we pass that to our SQL
 var customerChoice = function () {
-  inquirer.prompt ({
+  inquirer.prompt({
     name: "myProduct",
     type: "input",
     message: "Which item do you want to buy(enter the ID)?"
   }).then(function (anone) {
     var choice = anone.myProduct;
     connection.query(
-      "SELECT * from products where item_id=?", choice, 
+      "SELECT * from products where item_id=?", choice,
       function (err, res) {
         if (err) throw err;
         if (res.length === 0) {
+          //1st check to see if we got a value back
           //console.log(anone)
           console.log("Please enter a valid item");
           customerChoice();
         }
+        //now ask for the quantiry
         else {
           inquirer.prompt({
 
@@ -65,19 +63,25 @@ var customerChoice = function () {
             message: "How many would you like to buy?"
           })
             .then(function (antwo) {
-
+              //wait for a response
               var quantity = antwo.quantity;
               var updatedQuantity = res[0].stock_quantity - quantity;
+              var totalOrderPrice = quantity * res[0].price;
+              //this needs to be fixed as it is reducing all inventory
+              //need to add item id to where clause
+              //check to see if customer has ordered more than avail
               if (quantity > res[0].stock_quantity) {
                 console.log("We don't have sufficient stock. Please enter a lower quantity.")
                 customerChoice();
               }
+              //if there is enough then reduce the inventory by amount ordered - this is currently broken
               else {
-                connection.query("UPDATE products SET stock_quantity=?", updatedQuantity, 
-                function (err, res) {
-                  if (err) throw err;
-                })
-                console.log("We've fufilled your order.");
+                connection.query("UPDATE products SET stock_quantity  =? where item_id =?", [updatedQuantity, choice],
+                  function (err, res) {
+                    if (err) throw err;
+                  })                
+                console.log("We've fufilled your order.  It was " + totalOrderPrice);
+                process.exit(-1);; //exit once order complete
               }
             })
         }
