1feed75ae2e03e7c77d6bedc97f486cf7fcccb13
Paul Rogers <paulwrogers@yahoo.com>
2018-12-16 17:35:32 -0800

    Logic for customer complete withsome formatting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bamazon
 
+1. Please run the file Setup.sql in your mysql database to create the required DB objects
+2. You will need to run nmp install for mysql, inquirer and table before running the application
+3. 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,71 +1,90 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table');
- 
-// instantiate
-var table = new Table({
-    head: ['TH 1 label', 'TH 2 label']
-  , colWidths: [100, 200]
-});
- 
 
-// table is an Array, so you can `push`, `unshift`, `splice` and friends
+//instantiate
+
+// table is an Array, so you can `push`, `unshift`, `splice` and friends*/
 
 var connection = mysql.createConnection({
   host: "localhost",
-
   // Your port; if not 3306
   port: 3306,
-
   // Your username
   user: "root",
-
   // Your password
   password: "password",
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+//var itemReturned = []
+
+var showAllItems = function () {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    displayCatalog();
-    customerChoice();
-  });
-
-  function displayCatalog(){
-    var query = "SELECT * from products";
-    connection.query(query, function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        //put this back in once logic is done table.push(
-        //    [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price]
-      //  );
-        //console.log(table.toString());
-        //console.log(table)
-        console.log(res[i].item_id + res[i].product_name + res[i].department_name + res[i].price);
-        //console.log(res.length);
-      }
+    var table = new Table({
+      head: ['Product Id', 'Product Description', 'Cost'],
+      colWidths: [5, 50, 8],
     });
-  }
 
-  // need to ask questions on items and quntities
-    function customerChoice(){
-        inquirer
-            .prompt({
-                name: "item id",
-                type: "input",
-                message: "Which item do you want to buy?"
-            })
-
-            .prompt({
-                name: "quantity",
-                type: "input",
-                message: "How many would you like to buy?"
-            })
-        
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].price]);
     }
-    // need to check to see if there is enough quantity - if not print insufficient
-    // if there is enought then need to reduce the qunantity stored in the table by the amount entered
-    // then show the customer their total cost
 
-    
-  
+    //console.log(res[0].item_id )
+      //console.log(res[0].item_id )
+        //console.log(res[0].item_id )
+   console.log(table.toString());
+    customerChoice()
+  });
+};
+
+//neet to get item to add into inquirer question
+
+var customerChoice = function () {
+  inquirer.prompt ({
+    name: "myProduct",
+    type: "input",
+    message: "Which item do you want to buy(enter the ID)?"
+  }).then(function (anone) {
+    var choice = anone.myProduct;
+    connection.query(
+      "SELECT * from products where item_id=?", choice, 
+      function (err, res) {
+        if (err) throw err;
+        if (res.length === 0) {
+          //console.log(anone)
+          console.log("Please enter a valid item");
+          customerChoice();
+        }
+        else {
+          inquirer.prompt({
+
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to buy?"
+          })
+            .then(function (antwo) {
+
+              var quantity = antwo.quantity;
+              var updatedQuantity = res[0].stock_quantity - quantity;
+              if (quantity > res[0].stock_quantity) {
+                console.log("We don't have sufficient stock. Please enter a lower quantity.")
+                customerChoice();
+              }
+              else {
+                connection.query("UPDATE products SET stock_quantity=?", updatedQuantity, 
+                function (err, res) {
+                  if (err) throw err;
+                })
+                console.log("We've fufilled your order.");
+              }
+            })
+        }
+      })
+  })
+}
+
+
+showAllItems();
 
